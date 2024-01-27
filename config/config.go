516a652f3ec713062eab9239f1c4f3d6ee06e3c8
kamen.dimitrov1 <kamen.dimitrov@flaxandteal.co.uk>
2024-01-27 23:06:17 +0200

    fix: Fixed no timeout code security vulnerability

--- config/config.go
+++ config/config.go
@@ -1,12 +1,16 @@
 package config
 
 import (
+	"time"
+
 	"github.com/kelseyhightower/envconfig"
 )
 
 // Config represents service configuration for dp-search-scrubber-api
 type Config struct {
-	BindAddr string `envconfig:"BIND_ADDR"`
+	BindAddr     string        `envconfig:"BIND_ADDR"`
+	ReadTimeout  time.Duration `envconfig:"READ_TIMEOUT"`
+	WriteTimeout time.Duration `envconfig:"WRITE_TIMEOUT"`
 }
 
 var cfg *Config
@@ -15,7 +19,9 @@ var cfg *Config
 // variables
 func Get() (*Config, error) {
 	cfg = &Config{
-		BindAddr: ":3333",
+		BindAddr:     ":3333",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	return cfg, envconfig.Process("", cfg)
