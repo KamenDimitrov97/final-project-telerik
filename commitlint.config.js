ef2852b7f5678f4984da94e7ef60e2a36ccf975a
kamen.dimitrov1 <kamen.dimitrov@flaxandteal.co.uk>
2024-01-27 17:48:15 +0200

    fix: updated length of commit msgs

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-      'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'test', 'wip']],
-      'subject-case': [2, 'always', 'sentence-case'],
+      'type-enum': [48, 'always', ['feat', 'fix', 'docs', 'test', 'wip']],
+      'subject-case': [48, 'always', 'sentence-case'],
     },
   };
